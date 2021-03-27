79deb27e130a5616063221d044643de9cdd34806
jdavidm11 <80984771+jdavidm11@users.noreply.github.com>
2021-03-27 10:18:37 -0500

    .

--- lib/registrocentro.dart
+++ lib/registrocentro.dart
@@ -4,50 +4,73 @@ class RegistroCentro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Registro Centro Deportivo'),
-        ),
-        body: Center(
-          child: Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  
-                  SizedBox(
-                    height: 30,
-                    width: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                      width: 250,
-                      child: TextField(
-                        decoration: InputDecoration(
-                            labelText: "USUARIO", border: OutlineInputBorder()),
-                      ),
+      appBar: AppBar(
+        title: Text('Registro Centro Deportivo'),
+      ),
+      body: Center(
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: 250,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Nombre",
+                          border: OutlineInputBorder(),
+                          icon: Icon(Icons.account_circle)),
                     ),
                   ),
-                  SizedBox(
-                    height: 30,
-                    width: 30,
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: 250,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Usuario", border: OutlineInputBorder()),
+                    ),
                   ),
-                  Padding(
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: 250,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Contraseña",
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                ),
+                Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Container(
-                      width: 250,
-                      child: TextField(
-                        decoration: InputDecoration(
-                            labelText: "CONTRASEÑA",
-                            border: OutlineInputBorder()),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                        width: 250,
+                        child: TextField(
+                            decoration: InputDecoration(
+                                labelText: "Nombre del Centro Deportivo",
+                                border: OutlineInputBorder())))),
+              ],
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
